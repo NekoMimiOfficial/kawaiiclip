8bd3a12dc644ba80d38c2804e1de183e764e73e1
charlie. <167714837+NoMoreSyntaxx@users.noreply.github.com>
2025-10-13 15:17:20 +0000

    Update installer.sh

--- installer/installer.sh
+++ installer/installer.sh
@@ -1,5 +1,5 @@
 g++ kawaiiclip.cpp -o /usr/local/bin/kawaclip 
-mkdir -p ~/.config/kawaiiclip
+mkdir -p ~/.config/kawaiiclip/
 touch ~/.config/kawaiiclip/emoticons.txt
 echo ">w<\n>////<\n0_0\nqwq\n:3\n;3\nuwu\nX_X" > ~/.config/kawaiiclip/emoticons.txt
 

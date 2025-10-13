582b6597e95439d6a645573d97023703d4d44dda
charlie. <167714837+NoMoreSyntaxx@users.noreply.github.com>
2025-10-13 18:05:25 +0200

    Update installer.sh

--- installer/installer.sh
+++ installer/installer.sh
@@ -1,4 +1,3 @@
 g++ kawaiiclip.cpp -o /usr/local/bin/kawaclip 
 sudo mkdir -p /home/$USER/.config/kawaiiclip/
-sudo touch /home/$USER/.config/kawaiiclip/emoticons.txt
-sudo echo ">w<\n>////<\n0_0\nqwq\n:3\n;3\nuwu\nX_X" > /home/$USER/.config/kawaiiclip/emoticons.txt
+cp emoticons.txt /home/$USER/.config/kawaiiclip/emoticons.txt
